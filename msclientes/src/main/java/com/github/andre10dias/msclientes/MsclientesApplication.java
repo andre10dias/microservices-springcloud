c0ae6b50da21adb1ccbf655ad1798e370b6764c0
andre10dias <andre10dias@ymail.com>
2025-09-25 18:26:41 -0300

    Scalffolding do microserviço de clientes criado

---
+++ msclientes/src/main/java/com/github/andre10dias/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package com.github.andre10dias.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+
+}
