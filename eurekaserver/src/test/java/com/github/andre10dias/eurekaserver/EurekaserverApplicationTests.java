b8ba41ea0874f32aef4429f8cb340c6906f2f9e0
andre10dias <andre10dias@ymail.com>
2025-09-25 10:45:54 -0300

    Microserviço eurekaserver criado

---
+++ eurekaserver/src/test/java/com/github/andre10dias/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andre10dias.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
